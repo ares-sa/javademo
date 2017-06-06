1a65d1035ae4a1cfafc97b304a8acbe2186bffc8
ares-sa <ares-sa@qq.com>
2017-06-06 19:16:29 +0800

    ceshi

---
+++ src/cn/bdqn/tangcco/Huangshifeng2.java
@@ -0,0 +1,13 @@
+package cn.bdqn.tangcco;
+
+/**
+ * Created by Notebook on 2017/6/6.
+ */
+public class Huangshifeng2 {
+
+    public static void main(String[] args) {
+        System.out.println("456");
+    }
+
+
+}
